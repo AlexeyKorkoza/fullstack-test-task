09ba71df240802e384f445a94077f567d82314bb
Aleksei Korkoza <alexeykorkoza@gmail.com>
2025-11-16 20:45:14 +0300

    feature: remove unnecessary packages

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,7 +3,6 @@ import { AntdRegistry } from '@ant-design/nextjs-registry';
 import { type ReactNode } from 'react';
 
 import { Header } from '@/shared/Header';
-import Providers from 'app/providers';
 import { Layout } from '@/shared/Layout';
 import { Content } from '@/shared/Content';
 import './layout.scss';
@@ -12,14 +11,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Providers>
-          <AntdRegistry>
-            <Layout>
-              <Header />
-              <Content>{children}</Content>
-            </Layout>
-          </AntdRegistry>
-        </Providers>
+        <AntdRegistry>
+          <Layout>
+            <Header />
+            <Content>{children}</Content>
+          </Layout>
+        </AntdRegistry>
       </body>
     </html>
   );
